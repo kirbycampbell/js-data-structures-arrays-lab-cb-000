def73884721ba52834664a29bed3c81cb21dc493
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-13 16:38:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,8 @@ function appendDriver(name) {
   const newDrivers = [drivers[0], drivers[1], drivers[2], name];
   return newDrivers;
 }
+
+function prependDriver(name) {
+  const newDrivers = [name, drivers[0], drivers[1], drivers[2]];
+  return newDrivers;
+}
